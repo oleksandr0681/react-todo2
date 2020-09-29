a14116b68c850a8767a7c60bba64b5352cc7cef7
Alexander <test@test.com>
2020-09-30 02:59:31 +0300

    Updates

---
+++ precache-manifest.bf648e3ccb19bdb473dec6512c9e086e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da69bc90666b867a53a38095af1c33ad",
+    "url": "/react-todo2/index.html"
+  },
+  {
+    "revision": "f122772754623205365f",
+    "url": "/react-todo2/static/css/main.5a39f6bd.chunk.css"
+  },
+  {
+    "revision": "ce558c1d005a7ac643e3",
+    "url": "/react-todo2/static/js/2.3fde2acb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo2/static/js/2.3fde2acb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f122772754623205365f",
+    "url": "/react-todo2/static/js/main.546757fc.chunk.js"
+  },
+  {
+    "revision": "c94b85d9b01d358d4f85",
+    "url": "/react-todo2/static/js/runtime-main.d09fc897.js"
+  },
+  {
+    "revision": "df70b00589269cc7e903d0e3ba9db627",
+    "url": "/react-todo2/static/media/react.df70b005.png"
+  }
+]);
